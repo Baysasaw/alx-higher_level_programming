b390cbbdae6b9f4c174cabdc96c40df24c46ff8c
yonas <yonas988252656@gmail.com>
2024-01-16 18:58:55 +0300

    ASCII alphabet in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for x in range(122, 96, -2):
-    print(chr(x), end="")
-    print(chr(x  - 33),end="")
+for x in range(122, 96, -1):
+    if x % 2 ==0:
+        print(f"{chr(x)}",end="")
+    else:
+        print(f"{chr(x  - 32)}", end="")
  
