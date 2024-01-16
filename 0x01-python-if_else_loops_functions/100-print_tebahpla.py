64d0bf441ad2ac682057620b6955d0c7ee8024ee
yonas <yonas988252656@gmail.com>
2024-01-16 21:13:56 +0300

    python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for x in range(122, 96, -2):
-    print(f"{chr(x)}{chr(x  - 33)}", end="")
+for x in range(122, 96, -1):
+    if x % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(x - diff)), end='')
+
  
