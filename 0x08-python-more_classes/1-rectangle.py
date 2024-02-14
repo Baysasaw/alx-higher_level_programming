c03dc82d2b50a1635b2972f0d71c7be999310dec
yonas <104759558+Baysasaw@users.noreply.github.com>
2024-02-14 20:37:23 +0300

    simple rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,18 +9,23 @@ class Rectangle:
         self.height = height
         self.width = width
 
+    @property
     def width(self):
         return self.width
 
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
 
+    
+    @property
     def height(self):
         return self.height
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
