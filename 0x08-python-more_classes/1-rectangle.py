d61d4fa49f71eeffb81b7b20a404f4343328974d
yonas <104759558+Baysasaw@users.noreply.github.com>
2024-02-14 20:15:22 +0300

    Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    def width(self):
+        return self.width
+
+    def width(self, value):
+        if value is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    def height(self):
+        return self.height
+
+    def height(self, value):
+        if value is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
