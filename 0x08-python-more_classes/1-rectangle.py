c12e0c84e8c92284a0744724b2137aab440815e2
yonas <104759558+Baysasaw@users.noreply.github.com>
2024-02-14 21:06:12 +0300

    simple rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
     def width(self):
         return self.__width
 
-  
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -26,6 +26,7 @@ class Rectangle:
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
