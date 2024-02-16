c5008f2a66ba925ff9d5546870b4f39ba2b03820
yonas <yonas988252656@gmail.com>
2024-02-16 14:02:42 +0300

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" """
+def add_integer(a, b=98):
+    """a and b must be integers or floats, otherwise raise a TypeError
+    exception with the message a must be an integer or b must be an integer"""
+
+    if type(a) != int and type(a) != float:
+        raise TypeError('a must be an integer')
+
+    if type(b) != int and type(b) != float:
+        raise TypeError('b must be an integer')
+    else:
+        """a and b must be first casted to integers if they are float"""
+        a = int(a)
+        b = int(b)
+        """Returns an integer: the addition of a and b"""
+        return a + b
