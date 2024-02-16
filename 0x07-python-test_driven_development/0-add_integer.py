615a090fbbc3660487f67c32406e5ae9c14a2948
yonas <yonas988252656@gmail.com>
2024-02-16 16:15:03 +0300

    Real definition of a rectangle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
-"""A module to add two numbers
-
-This module performs the addition operation between two numbers,
-these numbers can be integers or floats.
-
-"""
 
 def add_integer(a, b=98):
-     """Adds two numbers
-
-    Performs the addition between two numbers.
-
-    Args:
-        a (:obj:`int, float`): The first number.
-        b (:obj:`int, float`, optional): The second number.
-
-    Returns:
-        int: The result of the addition.
-
-    """
-     
      
      if type(a) != int and type(a) != float:
         raise TypeError('a must be an integer')
