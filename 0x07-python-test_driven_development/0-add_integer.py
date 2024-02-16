78588404c43821f9f24c83ece9b934b35a5fb4cb
yonas <yonas988252656@gmail.com>
2024-02-16 14:54:18 +0300

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" """
+#0-add_integer.py
+
 def add_integer(a, b=98):
     """a and b must be integers or floats, otherwise raise a TypeError
     exception with the message a must be an integer or b must be an integer"""
