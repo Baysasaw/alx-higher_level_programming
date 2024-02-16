acdc72b94ea1cf0ca77d867aaf9fd18b10e8cc50
yonas <yonas988252656@gmail.com>
2024-02-16 21:41:50 +0300

    integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-
-Filename : 0-add_integer
-
-"""
-
 def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
