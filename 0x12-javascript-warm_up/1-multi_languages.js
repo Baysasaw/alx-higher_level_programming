040dcac310f2840bd40cf27fd25c91388200125e
baysasaw <yonas.baysasaw@aait.edu.et>
2024-08-20 16:40:58 +0300

    languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('avaScript is amazing');
