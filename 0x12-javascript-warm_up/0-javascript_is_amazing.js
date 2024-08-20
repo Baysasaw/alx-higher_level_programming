930af64c0cf5242aefae0f61a34aaa32e391f721
baysasaw <yonas.baysasaw@aait.edu.et>
2024-08-20 16:15:21 +0300

    First constant, first print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// Print string "javascript is amazint"
+// Print string “Javascript is amazing”
 
-const myVar = "JavaScript is amazing";
+const myVar = 'Javascript is amazing';
 console.log(myVar);
-
